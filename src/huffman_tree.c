74b07a89e80a4df5e497ee1d5dd62e7c6c39ea3f
Alexander Pitkin <peleccom@gmail.com>
2014-03-31 19:26:24 +0300

    Added generating tree from codes

--- src/huffman_tree.c
+++ src/huffman_tree.c
@@ -53,6 +53,7 @@ void walk(char *path, tree_node_t *node, codes_array_t *codes){
 		walk(right_path, node->right, codes);
 		FREE(right_path);
 	}
+	FREE(node);
 }
 
 codes_array_t* generate_codes_map(sorted_queue_t queue){
@@ -61,7 +62,7 @@ codes_array_t* generate_codes_map(sorted_queue_t queue){
 	codes = malloc(sizeof(codes_array_t));
 	for(i=0;i<256;i++)
 		codes->code[i] = NULL;
-	tree_node_t *root = sorted_queue_peek(queue);
+	tree_node_t *root = sorted_queue_pop(queue);
 	walk("", root, codes);
 	return codes;
 }
@@ -69,6 +70,7 @@ codes_array_t* generate_codes_map(sorted_queue_t queue){
 void free_codes(codes_array_t* codes){
 	int i;
 	for(i=0;i<256;i++)
+		//free strings
 		FREE(codes->code[i])
 	FREE(codes);
 }
@@ -98,4 +100,50 @@ codes_array_t* generate_codes(frequency_array_t *frequency_array){
 	}
 	sorted_queue_free(queue);
 	return codes;
+}
+
+
+tree_node_t* generate_tree(codes_array_t* codes){
+ // generate tree from codes_array
+    int i;
+    int j;
+    int l;
+    tree_node_t *root;
+    tree_node_t *cur_node;
+    tree_node_t *new_node;
+    char *s
+    root = malloc(sizeof(tree_node_t));
+    root->is_leaf = 1;
+    root->left = NULL;
+    root->right = NULL;
+    root->data = 0;
+    cur_node = root;
+    for(i=0; i<256; i++){
+      cur_node = root;
+      s = codes[i];
+      l = strlen(s);
+      for(j=0; j< l; j++){
+	 if (cur_node->is_leaf){
+	   cur_node->is_leaf = 0;
+	   new_node = malloc(sizeof(tree_node_t));	   
+	   if (s[j] == "1")
+	     cur_node->right = new_node;
+	   else
+	     cur_node->left = new_node;
+	   cur_node = new_node;
+	 }
+	 else
+	 {
+	   if (s[j] == "1")
+	     cur_node = cur_node->right;
+	   else
+	     cur_node = cur_node->left;
+	 }
+	 
+      }
+	 
+    
+      
+    }
+	
 }
